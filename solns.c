d804736862c197b8b2d76dc02fc0d16f057ebb3d
kidshalem <85776822+kidshalem@users.noreply.github.com>
2021-06-19 07:24:15 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,15 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+
+/*1*/
+
+float average(int s[], int l) {*
+    int sum = 0;
+    float avg = 0.0;
+    for (int i=0; i<l; i++) {
+        sum = sum + s[i];
+    }
+    avg = sum/l;
+    return avg;
+}     
+
